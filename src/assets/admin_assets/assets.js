118553af9fdddcdf91632160dac5fc46cacfd4c5
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-09-13 01:53:55 +0530

    add

--- src/assets/admin_assets/assets.js
+++ src/assets/admin_assets/assets.js
@@ -4,14 +4,17 @@ import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
-
+import basket_icon from './basket_icon.png';   
+import search_icona from './search_icona.png';
 export const assets ={
     logo,
     add_icon,
     order_icon,
     profile_image,
     upload_area,
-    parcel_icon
+    parcel_icon,
+    basket_icon,
+    search_icona,
 }
 
 export const url = 'http://localhost:4000'
