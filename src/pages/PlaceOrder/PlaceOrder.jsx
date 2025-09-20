348d0efc00345ff868be43a2136db16827777cbc
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-09-20 23:49:27 +0530

    add

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,33 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
-const PlaceOrder = () => {
+import { StoreContext } from '../../context/StoreContext'
+
+const PlaceOrder = () => { 
+
+  const {getTotalCartAmout} = useContext(StoreContext)
   return (
-    <form  className="place-order">
+    <form className="place-order">
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-friends">
-          <input type="text" placeholder='First Name' />
-          <input type="text" placeholder='Last Name' />
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
         </div>
-        <input type="text" placeholder='Email address' />
-        <input type="text" placeholder='Street'/>
+        <input type="text" placeholder="Email address" />
+        <input type="text" placeholder="Street" />
 
         <div className="multi-friends">
-          <input type="text" placeholder='City' />
-          <input type="text" placeholder='State' />
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
         </div>
         <div className="multi-friends">
-          <input type="text" placeholder='Zip code' />
-          <input type="text" placeholder='Country' />
+          <input type="text" placeholder="Zip code" />
+          <input type="text" placeholder="Country" />
         </div>
-        <input type="text" placeholder='Phone' />
+        <input type="text" placeholder="Phone" />
       </div>
-    
-    <div className="place-order-right">
-          <div className="cart-total">
-            <h2>Cart Totals</h2>
-          </div>
-          <div className="cart-total-details">
+
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+        </div>
+ <div className="cart-total-details">
             <p>Subtotal</p>
             <p>${getTotalCartAmount()}</p>
             
@@ -35,18 +39,17 @@ const PlaceOrder = () => {
           <hr />
           <div className="cart-total-details">
             <p>Delivery Fee</p>
-            <p>${2}</p>
+            <p>${getTotalCartAmount()===0?0:2}</p>
           
           </div>
             <hr />
           <div className="cart-total-details">
             <b>Total</b>
-            <b>${getTotalCartAmount()+2}</b>
+            <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
           </div>
-        </div>
-        <button >PROCEED TO PAYMENT</button>
       </div>
-    </div>
+
+      <button>PROCEED TO PAYMENT</button>
     </form>
   )
 }
